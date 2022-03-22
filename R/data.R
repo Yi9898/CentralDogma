497a3e698b5da779305402f63ca0e70cd47b1f6a
Yi9898 <yihuang9898@gmail.com>
2022-03-22 16:26:56 +0100

    create codon_table and transcription funtion

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Codon Table
+#'
+#' It is a table showing all the codons and their corresponding amino acids
+#'
+#' @format
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
